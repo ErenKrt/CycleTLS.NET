54ffc18a1d8ddcfe002a629a54092ece933a3b1a
Eren <kurteren07@gmail.com>
2023-12-10 16:49:24 +0300

    Prepaing for publish

---
+++ src/CycleTLS/Helpers/WebProxyExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace CycleTLS.Helpers
+{
+    public static class WebProxyExtensions
+    {
+        public static string toStringWithCredentials(this WebProxy proxy)
+        {
+            var proxyURL = proxy.GetProxy(new Uri("http://example.com"));
+            var creds = proxy.Credentials as NetworkCredential;
+
+            return creds != null
+                ? $"{proxyURL.Scheme}://{creds.UserName}:{creds.Password}@{proxyURL.Authority}"
+                : $"{proxyURL.Scheme}://{proxyURL.Authority}";
+        }
+    }
+}
